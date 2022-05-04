096bbc0432bba32eb7c9761240d09953094e3b15
Aleksandr Tugolukov <billgadov3k@gmail.com>
2022-05-04 19:50:15 +0300

    add  sorting by group, sorting by last name

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const App = {
     data() {
       return {
+        revers: true,
         numberGroup: 0,
         firstName: "",
         midleName: "",
@@ -12,7 +13,7 @@ const App = {
             firstName: "Aleksandr",
             midleName: "Vladimirovich",
             lastname: "Tugolukov",
-            numberGroup: "1",
+            numberGroup: "1330",
             gender: "male",
             date: "01.01.2022"
           },
@@ -20,8 +21,23 @@ const App = {
             firstName: "Vasya",
             midleName: "Gennadievich",
             lastname: "Ivanov",
-            numberGroup: "2",
+            numberGroup: "1122",
             gender: "famale",
+            date: "01.01.2022"
+          },{
+            firstName: "Nikita",
+            midleName: "Sergeevich",
+            lastname: "Minin",
+            numberGroup: "1222",
+            gender: "male",
+            date: "01.01.2022"
+          },
+          {
+            firstName: "Misha",
+            midleName: "Petrovich",
+            lastname: "Dyatlov",
+            numberGroup: "1233",
+            gender: "male",
             date: "01.01.2022"
           }
         ]
@@ -66,8 +82,34 @@ const App = {
       //сортировка по номеру группы
       sortNumberGroup(){
         let student = this.students;
-        console.log(student);
-      }
+        if (this.revers) {
+          student.sort(function(a, b){
+            return a.numberGroup - b.numberGroup; 
+          });
+          this.students = student;
+          this.revers = !this.revers;
+        } else {
+          student.sort(function(a, b){
+            return b.numberGroup - a.numberGroup; 
+          });
+          this.students = student;
+          this.revers = !this.revers;
+        }
+        // console.log(this.revers);
+      },
+      // Сортировка по фамилии
+      sortLastName(){
+        let student = this.students;
+        if (this.revers) {
+          student.sort((a, b) => a.lastname > b.lastname ? 1 : -1);
+          this.revers = !this.revers;
+        } else {
+          student.sort((a, b) => a.lastname < b.lastname ? 1 : -1);
+          this.revers = !this.revers;
+        }
+        // console.log(student);
+        // console.log(this.revers);
+      },
     }
   }
 
